28a5413727fa26fbde601eb4a3ce4a82955f0139
matins85 <olorunsholamatins@gmail.com>
2025-06-05 17:32:49 +0100

    Fix: Move django.setup() before importing channels modules

--- aerofinder/asgi.py
+++ aerofinder/asgi.py
@@ -1,28 +1,16 @@
-"""
-ASGI config for app project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
 import os
-import django  # 👈 add this
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-
+import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aerofinder.settings')
-django.setup()  # 👈 critical to fix the AppRegistryNotReady error
+django.setup()
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 from accounts.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
+        URLRouter(websocket_urlpatterns)
     ),
 })
