a481056289dd7b66d2baf85ad364e1b2fce0d334
matins85 <olorunsholamatins@gmail.com>
2025-06-05 17:26:34 +0100

    fixes

--- aerofinder/asgi.py
+++ aerofinder/asgi.py
@@ -8,12 +8,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-from django.core.asgi import get_asgi_application
+import django  # 👈 add this
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from accounts.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aerofinder.settings')
+django.setup()  # 👈 critical to fix the AppRegistryNotReady error
+
+from django.core.asgi import get_asgi_application
+from accounts.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
